7fb74b51b9205df787cb249ac914103ac467f948
Artem Martiyanov <artem-martiyanov@mail.ru>
2023-06-30 23:24:58 +0300

    Adds main

--- source/js/index.js
+++ source/js/index.js
@@ -1,10 +1,11 @@
-
 document.addEventListener('pointerdown', (evt) => {
   const dataset = evt.target.dataset
   
   if (dataset.type === 'burger') {
     const burgerButton = evt.target
     burgerButton.classList.toggle('burger--active')
+    
+    document.body.classList.toggle('page--locked')
     
     const subHeader = document.querySelector('.header__sub-header')
     subHeader.classList.toggle('header__sub-header--active')
